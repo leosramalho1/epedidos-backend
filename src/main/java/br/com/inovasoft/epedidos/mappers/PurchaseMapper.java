e9c3eea7658ce5d708e20f50d310a93e3ee72c8c
Gabriel <gabrielfc@gmail.com>
2021-01-26 15:32:36 -0300

    Correção formato data

--- src/main/java/br/com/inovasoft/epedidos/mappers/PurchaseMapper.java
+++ src/main/java/br/com/inovasoft/epedidos/mappers/PurchaseMapper.java
@@ -2,10 +2,18 @@ package br.com.inovasoft.epedidos.mappers;
 
 import br.com.inovasoft.epedidos.models.dtos.PurchaseDto;
 import br.com.inovasoft.epedidos.models.entities.Purchase;
+
+import java.util.List;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "cdi", uses = { SupplierMapper.class, PurchaseItemMapper.class, UserPortalMapper.class })
 public interface PurchaseMapper extends BaseMapper<Purchase, PurchaseDto> {
 
-
+    @Mappings({
+        @Mapping(target = "dueDate", dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    })
+    List<Purchase> toEntity(List<PurchaseDto> dto);
 }
