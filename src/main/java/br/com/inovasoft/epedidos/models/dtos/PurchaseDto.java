a032b2f11142ce008beb1c6caeb2a9e5248123d7
Gabriel <gabrielfc@gmail.com>
2021-01-27 15:48:36 -0300

    Correção payload

--- src/main/java/br/com/inovasoft/epedidos/models/dtos/PurchaseDto.java
+++ src/main/java/br/com/inovasoft/epedidos/models/dtos/PurchaseDto.java
@@ -1,17 +1,14 @@
 package br.com.inovasoft.epedidos.models.dtos;
 
-import br.com.inovasoft.epedidos.models.dtos.serializers.MoneySerializer;
-import br.com.inovasoft.epedidos.models.enums.OrderEnum;
+import java.math.BigDecimal;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import br.com.inovasoft.epedidos.models.dtos.serializers.MoneySerializer;
+import br.com.inovasoft.epedidos.models.enums.OrderEnum;
 import lombok.Data;
-import org.apache.commons.collections.CollectionUtils;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.List;
-import java.util.Objects;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -31,27 +28,8 @@ public class PurchaseDto {
     private String payMethod;
     @JsonSerialize(using = MoneySerializer.class)
     private BigDecimal totalValue;
-
-  
     @JsonSerialize(using = MoneySerializer.class)
-    public BigDecimal getAverageValue() {
-        if(CollectionUtils.isNotEmpty(itens)) {
-            Integer quantityTemp = getTotalQuantity();
-            return getTotalValue()
-                    .divide(BigDecimal.valueOf(quantityTemp>0?quantityTemp:1), 2, RoundingMode.HALF_UP);
-        }
-
-        return BigDecimal.ZERO;
-    }
-
-    public Integer getTotalQuantity() {
-        if(CollectionUtils.isNotEmpty(itens)) {
-            return itens.stream()
-                    .map(PurchaseItemDto::getQuantity)
-                    .filter(Objects::nonNull)
-                    .reduce(0, Integer::sum);
-        }
-
-        return 0;
-    }
+    private BigDecimal averageValue;
+    @JsonSerialize(using = MoneySerializer.class)
+    private BigDecimal totalQuantity;
 }
