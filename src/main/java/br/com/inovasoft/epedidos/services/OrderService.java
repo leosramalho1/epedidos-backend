4d9852618857ff2dce298dca2dc6d8ccedce8bda
Gabriel <gabrielfc@gmail.com>
2021-02-08 20:39:36 -0300

    Correção consulta pedidos por conta a receber

--- src/main/java/br/com/inovasoft/epedidos/services/OrderService.java
+++ src/main/java/br/com/inovasoft/epedidos/services/OrderService.java
@@ -118,7 +118,7 @@ public class OrderService extends BaseService<Order> {
 
     public List<OrderDto> listAllByCustomerAndAccounttoReceive(Long idAccountToReceive) {
         PanacheQuery<Order> listOrders = Order.find(
-            "select p from Order p where p.systemId = ?1 and p.customer.cpfCnpj=?2 and p.accountToReceive.id=?3 and p.deletedOn is null order by p.id desc", tokenService.getSystemId(),tokenService.getJsonWebToken().getSubject(),idAccountToReceive);
+            "select p from Order p, PurchaseDistribution d where d.orderItem.id = p.id and p.systemId = ?1 and p.customer.cpfCnpj=?2 and d.accountToReceive.id=?3 and p.deletedOn is null order by p.id desc", tokenService.getSystemId(),tokenService.getJsonWebToken().getSubject(),idAccountToReceive);
 
 		return mapper.toDto(listOrders.list());
 	}
