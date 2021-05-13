b2e150cd4ca3b50646737ef7ef87dd77bed12fe6
Gabriel <gabrielfc@gmail.com>
2021-05-12 22:01:29 -0300

    Correção data atualização

--- src/main/java/br/com/inovasoft/epedidos/services/OrderService.java
+++ src/main/java/br/com/inovasoft/epedidos/services/OrderService.java
@@ -55,13 +55,13 @@ public class OrderService extends BaseService<Order> {
 
     public PaginationDataResponse<OrderDto> listAll(int page, List<OrderEnum> orderEnums) {
 
-        if(CollectionUtils.isEmpty(orderEnums)) {
+        if (CollectionUtils.isEmpty(orderEnums)) {
             orderEnums = List.of(OrderEnum.values());
         }
 
         String query = "deletedOn is null and systemId = ?1 and status in (?2)";
-        PanacheQuery<Order> listOrders = Order.find(
-                query, Sort.by("id").descending(), tokenService.getSystemId(), orderEnums);
+        PanacheQuery<Order> listOrders = Order.find(query, Sort.by("id").descending(), tokenService.getSystemId(),
+                orderEnums);
 
         List<Order> dataList = listOrders.page(Page.of(page - 1, LIMIT_PER_PAGE)).list();
 
@@ -69,11 +69,9 @@ public class OrderService extends BaseService<Order> {
                 (int) Order.count(query, tokenService.getSystemId(), orderEnums));
     }
 
-
     public Order findById(Long id) {
-        return Order.find("select p from Order p where p.id = ?1 " +
-                        "and p.systemId = ?2 and p.deletedOn is null",
-                id, tokenService.getSystemId()).firstResult();
+        return Order.find("select p from Order p where p.id = ?1 " + "and p.systemId = ?2 and p.deletedOn is null", id,
+                tokenService.getSystemId()).firstResult();
     }
 
     public OrderDto findDtoById(Long id) {
@@ -117,14 +115,15 @@ public class OrderService extends BaseService<Order> {
         return mapper.toDto(entity);
     }
 
-
     @Transactional
     public OrderDto update(Long id, OrderDto dto) {
         Order entity = Order.findById(id);
         OrderItem.delete("order.id = ?1", id);
         List<OrderItem> orderItems = orderItemMapper.toEntity(dto.getItens());
         saveOrderItems(entity, orderItems);
+        LocalDateTime createdOn = entity.getCreatedOn();
         mapper.updateEntityFromDto(dto, entity);
+        entity.setCreatedOn(createdOn);
         entity.setStatus(dto.getStatus());
         entity.persist();
         return mapper.toDto(entity);
@@ -135,60 +134,54 @@ public class OrderService extends BaseService<Order> {
         Order.update("set deletedOn = now() where id = ?1 and systemId = ?2", id, tokenService.getSystemId());
     }
 
-	public List<OrderDto> listAllByCustomer() {
+    public List<OrderDto> listAllByCustomer() {
         PanacheQuery<Order> listOrders = Order.find(
-            "select p from Order p where p.systemId = ?1 " +
-                    "and p.customer.cpfCnpj=?2 " +
-                    "and p.deletedOn is null " +
-                    "order by p.id desc", tokenService.getSystemId(), tokenService.getJsonWebToken().getSubject());
+                "select p from Order p where p.systemId = ?1 " + "and p.customer.cpfCnpj=?2 "
+                        + "and p.deletedOn is null " + "order by p.id desc",
+                tokenService.getSystemId(), tokenService.getJsonWebToken().getSubject());
 
-		return mapper.toDto(listOrders.list());
-	}
+        return mapper.toDto(listOrders.list());
+    }
 
     public List<OrderDto> listAllByCustomerAndAccounttoReceive(Long idAccountToReceive) {
         PanacheQuery<Order> listOrders = Order.find(
-            "select p from Order p, PurchaseDistribution d where d.orderItem.id = p.id and p.systemId = ?1 and p.customer.cpfCnpj=?2 and d.accountToReceive.id=?3 and p.deletedOn is null order by p.id desc", tokenService.getSystemId(),tokenService.getJsonWebToken().getSubject(),idAccountToReceive);
+                "select p from Order p, PurchaseDistribution d where d.orderItem.id = p.id and p.systemId = ?1 and p.customer.cpfCnpj=?2 and d.accountToReceive.id=?3 and p.deletedOn is null order by p.id desc",
+                tokenService.getSystemId(), tokenService.getJsonWebToken().getSubject(), idAccountToReceive);
 
-		return mapper.toDto(listOrders.list());
-	}
-
+        return mapper.toDto(listOrders.list());
+    }
 
     private void saveOrderItems(Order entity, List<OrderItem> orderItems) {
         if (CollectionUtils.isNotEmpty(orderItems)) {
-            orderItems.stream()
-                    .filter(o -> o.getQuantity() > 0)
-                    .forEach(orderItem -> {
-                        orderItem.setId(null);
-                        orderItem.setOrder(entity);
-                        if(Objects.isNull(orderItem.getProduct().getShippingCost())) {
-                            orderItem.setProduct(Product.findById(orderItem.getProduct().getId()));
-                        }
-                        orderItem.setUnitShippingCost(orderItem.getProduct().getShippingCost());
-                        OrderItem.persist(orderItem);
-                    });
+            orderItems.stream().filter(o -> o.getQuantity() > 0).forEach(orderItem -> {
+                orderItem.setId(null);
+                orderItem.setOrder(entity);
+                if (Objects.isNull(orderItem.getProduct().getShippingCost())) {
+                    orderItem.setProduct(Product.findById(orderItem.getProduct().getId()));
+                }
+                orderItem.setUnitShippingCost(orderItem.getProduct().getShippingCost());
+                OrderItem.persist(orderItem);
+            });
 
         }
     }
 
     @Transactional
     public int changeOrdersToStatusPurchase(Long systemId) {
-        return Order.update("set status = ?1 where status = ?2 and systemId = ?3", OrderEnum.PURCHASE, OrderEnum.OPEN, systemId);
+        return Order.update("set status = ?1 where status = ?2 and systemId = ?3", OrderEnum.PURCHASE, OrderEnum.OPEN,
+                systemId);
     }
 
     @Transactional
     public OrderDto changeOrdersToStatusPurchase() {
         int updates = changeOrdersToStatusPurchase(tokenService.getSystemId());
-        return OrderDto.builder()
-                .updates(updates)
-                .build();
+        return OrderDto.builder().updates(updates).build();
     }
 
     @Transactional
     public void closeOrders(List<CustomerBillingDto> customerBillingDtos) {
 
-        customerBillingDtos.stream()
-                .map(CustomerBillingDto::getPurchaseDistributions)
-                .flatMap(Collection::stream)
+        customerBillingDtos.stream().map(CustomerBillingDto::getPurchaseDistributions).flatMap(Collection::stream)
                 .forEach(purchaseDistributionDto -> {
                     Order order = Order.findById(purchaseDistributionDto.getIdOrder());
                     packageLoanService.registryPackageLoan(order.getOrderItems());
@@ -201,22 +194,14 @@ public class OrderService extends BaseService<Order> {
     public void schedulerOrderToPurchase(@NotNull String cron, @NotNull Long systemId) throws SchedulerException {
         String cronQuartz = cronUnixToCronQuartz(cron);
 
-        JobKey jobKey = JobKey.jobKey("order-job-" + systemId,
-                "system-" + systemId);
+        JobKey jobKey = JobKey.jobKey("order-job-" + systemId, "system-" + systemId);
 
-        JobDetail job = JobBuilder.newJob(OrderToPurchaseJob.class)
-                .withIdentity(jobKey)
-                .build();
+        JobDetail job = JobBuilder.newJob(OrderToPurchaseJob.class).withIdentity(jobKey).build();
 
-        job.getJobDataMap()
-                .put("systemId", systemId);
+        job.getJobDataMap().put("systemId", systemId);
 
-        Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity("order-trigger-" + systemId,
-                        "system-" + systemId)
-                .withSchedule(CronScheduleBuilder.cronSchedule(cronQuartz))
-                .startNow()
-                .build();
+        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("order-trigger-" + systemId, "system-" + systemId)
+                .withSchedule(CronScheduleBuilder.cronSchedule(cronQuartz)).startNow().build();
 
         if (quartz.checkExists(jobKey)) {
             quartz.deleteJob(jobKey);
